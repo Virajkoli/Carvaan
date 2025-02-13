5e609a4807063ea49b9c761d3d2581b6c81f2b74
Pratik Vijay Nikam <2241038@gcoej.ac.in>
2025-02-13 09:28:26 +0530

    navbar

--- Carvaan_2K25/js/script.js
+++ Carvaan_2K25/js/script.js
@@ -1,13 +1,20 @@
-const menuBtn = document.getElementById("menu-btn");
-const mobileMenu = document.getElementById("mobile-menu");
-const closeMenu = document.getElementById("close-menu");
+// const menuBtn = document.getElementById("menu-btn");
+// const mobileMenu = document.getElementById("mobile-menu");
+// const closeMenu = document.getElementById("close-menu");
 
-menuBtn.addEventListener("click", () => {
-  mobileMenu.classList.remove("menu-close");
-  mobileMenu.classList.add("menu-open");
+// menuBtn.addEventListener("click", () => {
+//   mobileMenu.classList.remove("menu-close");
+//   mobileMenu.classList.add("menu-open");
+// });
+
+// closeMenu.addEventListener("click", () => {
+//   mobileMenu.classList.remove("menu-open");
+//   mobileMenu.classList.add("menu-close");
+// });
+
+document.getElementById("menu-icon").addEventListener("click", function() {
+  document.getElementById("nav-links").classList.toggle("active");
+  document.body.classList.toggle("menu-open");
+  document.querySelector(".overlay").classList.toggle("active");
 });
 
-closeMenu.addEventListener("click", () => {
-  mobileMenu.classList.remove("menu-open");
-  mobileMenu.classList.add("menu-close");
-});
